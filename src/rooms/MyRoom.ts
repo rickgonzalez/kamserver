e578725ec10717041dd8c6d4131b8aa41357631d
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-11-04 16:50:12 -0400

    trying http just to not give up

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -1,6 +1,8 @@
 import { Room, Client} from "@colyseus/core";
 import { MyRoomState } from "./schema/MyRoomState";
 const requestIp = require('request-ip');
+import http from "http";
+
 
 export class MyRoom extends Room<MyRoomState> {
   maxClients = 4;
@@ -14,8 +16,9 @@ export class MyRoom extends Room<MyRoomState> {
       //
     });
   }
- onAuth (client: Client, options: any, request: any ) {
-     // request.socket.remoteAddress ? clientIp = request.socket.remoteAddress : clientIp = request.headers['x-forwarded-for'];
+ onAuth (client: Client, options: any, request: http.IncomingMessage ) {
+     //request previously set to any
+  // request.socket.remoteAddress ? clientIp = request.socket.remoteAddress : clientIp = request.headers['x-forwarded-for'];
     
      const clientIp = requestIp.getClientIp(request); 
       client.send("ip", clientIp);
