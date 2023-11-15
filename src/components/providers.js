fa1de54d0192d2bd7894301208bc40981691d605
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-11-15 17:28:22 -0500

    provider data type change

--- src/components/providers.js
+++ src/components/providers.js
@@ -1,8 +1,7 @@
 
 
 export const providerData = {
-    "Data":{
-    "providers":[
+    "Data":[
     {
         "providerName":"Kamserver development",
         "providerId":"15ad2302-1928-47c4-8b61-75f748a6b382",
@@ -15,8 +14,8 @@ export const providerData = {
         "providerUrl":"https://us-atl-3b185468.colyseus.cloud",
         "providerPort":3000
     }
-            ]
-    }
+    ]
+ 
 }
     
 export default providerData;
