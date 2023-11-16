b8938c926de7827a81faf8f5e1c9ed365041942d
Rick Gonzalez <rick.gonzalez68@gmail.com>
2023-11-15 23:35:18 -0500

    more providers

--- src/components/providers.js
+++ src/components/providers.js
@@ -3,13 +3,22 @@
 export const providerData = {
     "Data":[
     {
+        "id":1,
         "providerName":"Kamserver development",
         "providerId":"15ad2302-1928-47c4-8b61-75f748a6b382",
         "providerUrl":"https://us-atl-3b185468.colyseus.cloud",
         "providerPort":3000
     },
     {
+        "id":2,
         "providerName":"Some Other cool server",
+        "providerId":"15ad2302-1928-47c4-8b61-75f748a6b382",
+        "providerUrl":"https://us-atl-3b185468.colyseus.cloud",
+        "providerPort":3000
+    },
+    {
+        "id":3,
+        "providerName":"San Marcos",
         "providerId":"15ad2302-1928-47c4-8b61-75f748a6b382",
         "providerUrl":"https://us-atl-3b185468.colyseus.cloud",
         "providerPort":3000
